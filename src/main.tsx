ec2a89dcce0748faa65a47f4ab572f5c2c2fad4e
arthouse <146998950+yusufsafary@users.noreply.github.com>
2026-06-24 12:54:36 +0700

    Update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
+);
